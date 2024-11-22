d26fc10638d3d04f2c7fd86b721c10ef5c62a6b9
margobaklan <margo.baklan@gmail.com>
2024-11-22 19:44:11 +0200

    upd

--- data.py
+++ data.py
@@ -4,9 +4,6 @@ from cl import *
 
 GROUPS = [Group('МІ-41', 20),
           Group('МІ-42', 22)]
-        #   Group('ТК-41', 18),
-        #   Group('ТТП-41', 30),
-        #   Group('ТТП-42', 28)]
 
 AUDITORIUMS = [Auditorium('43', 60),
                Auditorium('235', 30),
@@ -45,13 +42,8 @@ for key, value in subject_and_type.items():
 SUBJECTS = []
 
 for group in GROUPS:
-    # subjects_and_types = random.sample(subject_and_type_list, int(len(subject_and_type_list)*0.6))
     for sat in subject_and_type_list:
         SUBJECTS.append(Subject(sat[0], group, 1, 1))
-        # if len(sat[1]) == 1:
-        #     SUBJECTS.append(Subject(sat[0], group, 2, 0))
-        # else:
-        #     SUBJECTS.append(Subject(sat[0], group, 2, 2))
 
 TIMESLOTS = ['08:40', '10:35', '12:20']
 DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
